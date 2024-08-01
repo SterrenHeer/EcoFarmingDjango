159d7f868832ff3a73468b6c6631c6a9f179e35b
SterrenHeer <sterrenheer@gmail.com>
2024-08-01 11:00:53 +0300

    add harmful page for links

--- eco_farming/urls.py
+++ eco_farming/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", views.WeatherSlideListView.as_view(), name="index"),
-    path('products/categories/', views.ProductCategoryView.as_view(), name='product_categories'),
+    path('products/categories/<str:type>', views.ProductCategoryView.as_view(), name='product_categories'),
+    path("harmful/categories/<str:type>", TemplateView.as_view(template_name="harmful.html"), name="harmful_categories"),
     path('product_brands/<str:type>/', views.BrandsListView.as_view(), name='product_brands'),
     path('brand_page/details/<int:pk>/<str:type>/', views.BrandDetailView.as_view(), name='brands_details'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
